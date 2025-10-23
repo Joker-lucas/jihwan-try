1824c92543fbd03b7482e4b5c4f765065c6539ba
김지환 <alfmah0481@gmail.com>
2025-10-23 23:50:34 +0900

    [#45] admin,user 부분 수정하기

--- expense-tracker/src/controllers/user.js
+++ expense-tracker/src/controllers/user.js
@@ -6,73 +6,9 @@ const { getLogger } = require('../libs/logger');
 const { successResponse } = response;
 const { CustomError } = error;
 const { ERROR_CODES } = errorDefinition;
-const { isAdmin } = authUtils;
+const { isAdmin, isSelfOrAdmin } = authUtils;
 
 const logger = getLogger('controllers/user.js');
-
-const getMyProfile = async (req, res, next) => {
-  try {
-    const userId = req.user.userId;
-    logger.info('내 프로필 조회 요청');
-
-    const user = await userService.getUserById(userId);
-
-    if (!user) {
-      throw new CustomError(ERROR_CODES.USER_NOT_FOUND);
-    }
-
-    const userPayload = {
-      nickname: user.nickname,
-      contactEmail: user.contactEmail,
-      profileImageUrl: user.profileImageUrl,
-      gender: user.gender,
-      birthday: user.birthday,
-      level: user.level,
-      exp: user.exp
-    };
-
-
-    successResponse(res, userPayload);
-  } catch (error) {
-    logger.error(error, '내 프로필 조회 중 에러 발생');
-    throw error;
-  }
-};
-
-const updateMyProfile = async (req, res, next) => {
-  try {
-    const userId = req.user.userId;
-    logger.info({ userId, body: req.body }, '내 프로필 수정 요청');
-
-    const updatedUser = await userService.updateUserById(userId, req.body);
-
-    if (!updatedUser) {
-      throw new CustomError(ERROR_CODES.USER_NOT_FOUND);
-    }
-
-    const updateUserPayload = {
-      nickname: updatedUser.nickname,
-      contactEmail: updatedUser.contactEmail,
-      profileImageUrl: updatedUser.profileImageUrl,
-      gender: updatedUser.gender,
-      birthday: updatedUser.birthday,
-    };
-    successResponse(res, updateUserPayload);
-  } catch (error) {
-    logger.error(error, '내 프로필 수정 중 에러 발생');
-    throw error;
-  }
-};
-
-const deleteMyAccount = async (req, res, next) => {
-  try {
-
-    await userService.deleteUserById(req.user.userId);
-    successResponse(res, { message: '계정이 성공적으로 삭제되었습니다.' });
-  } catch (error) {
-    next(error);
-  }
-};
 
 
 const getAllUsers = async (req, res, next) => {
@@ -82,7 +18,19 @@ const getAllUsers = async (req, res, next) => {
     }
 
     const users = await userService.getAllUsers();
-    successResponse(res, users);
+
+    const usersPayload = users.map(user => ({
+      nickname: user.nickname, 
+      contactEmail: user.contactEmail,
+      profileImageUrl: user.profileImageUrl,
+      gender: user.gender,
+      birthday: user.birthday,
+      level: user.level,
+      exp: user.exp,
+      role: user.role
+    }));
+
+    successResponse(res, usersPayload);
   } catch (error) {
     throw error;
   }
@@ -91,16 +39,20 @@ const getAllUsers = async (req, res, next) => {
 const getUserById = async (req, res, next) => {
   try {
 
-    if (!isAdmin(req.user)) {
-      throw new CustomError(ERROR_CODES.FORBIDDEN);
-    }
+    const requester = req.user;
+    const targetUserId = req.params.userId; 
 
-    const user = await userService.getUserById(req.params.userId);
+    const user = await userService.getUserById(targetUserId);
+
     if (!user) {
       throw new CustomError(ERROR_CODES.USER_NOT_FOUND);
     }
 
-    const userAllPayload = {
+    if (!isSelfOrAdmin(requester, targetUserId)) {
+      throw new CustomError(ERROR_CODES.FORBIDDEN);
+    }
+
+    const userPayload = {
       nickname: user.nickname,
       contactEmail: user.contactEmail,
       profileImageUrl: user.profileImageUrl,
@@ -111,7 +63,7 @@ const getUserById = async (req, res, next) => {
       role: user.role
     };
 
-    successResponse(res, userAllPayload);
+    successResponse(res, userPayload);
 
   } catch (error) {
     logger.error(error, '내 프로필 조회 중 에러 발생');
@@ -124,23 +76,33 @@ const getUserById = async (req, res, next) => {
 const updateUserById = async (req, res, next) => {
   try {
 
-    if (!isAdmin(req.user)) {
+    const requester = req.user;
+    const targetUserId = req.params.userId;
+    const updateData = req.body;
+
+    const user = await userService.getUserById(targetUserId);
+
+    if (!user) {
+        throw new CustomError(ERROR_CODES.USER_NOT_FOUND);
+    }
+
+    if (!isSelfOrAdmin(requester, targetUserId)) {
       throw new CustomError(ERROR_CODES.FORBIDDEN);
     }
 
-    const updatedUser = await userService.updateUserById(req.params.userId, req.body);
-    if (!updatedUser) {
-      throw new CustomError(ERROR_CODES.USER_NOT_FOUND);      
+    if (updateData.role && !isAdmin(requester)) {
+      throw new CustomError(ERROR_CODES.FORBIDDEN);
     }
 
+    await userService.updateUserById(targetUserId, updateData);
 
     const updateUserAllPayload = {
-      nickname: updatedUser.nickname,
-      contactEmail: updatedUser.contactEmail,
-      profileImageUrl: updatedUser.profileImageUrl,
-      gender: updatedUser.gender,
-      birthday: updatedUser.birthday,
-      role: updatedUser.role
+      nickname: user.nickname,
+      contactEmail: user.contactEmail,
+      profileImageUrl: user.profileImageUrl,
+      gender: user.gender,
+      birthday: user.birthday,
+      role: user.role
     };
 
     successResponse(res, updateUserAllPayload);
@@ -153,27 +115,29 @@ const updateUserById = async (req, res, next) => {
 
 const deleteUserById = async (req, res, next) => {
   try {
-    if (!isAdmin(req.user)) {
+
+    const requester = req.user;
+    const targetUserId = req.params.userId;
+
+    const userExists = await userService.getUserById(targetUserId);
+    if (!userExists) {
+        throw new CustomError(ERROR_CODES.USER_NOT_FOUND);
+    }
+
+    if (!isSelfOrAdmin(requester, targetUserId)) {
       throw new CustomError(ERROR_CODES.FORBIDDEN);
     }
 
-    const isDeleted = await userService.deleteUserById(req.params.userId);
-    if (!isDeleted) {
-      throw new CustomError(ERROR_CODES.USER_NOT_FOUND);
-    }
+    await userService.deleteUserById(targetUserId);
+
     successResponse(res, { message: '성공적으로 삭제되었습니다.' });
   } catch (error) {
-    logger.error(error, '내 프로필 수정 중 에러 발생');
     throw error;
   }
 };
 
 
 module.exports = {
-  getMyProfile,
-  updateMyProfile,
-  deleteMyAccount,
-
   getAllUsers,
   getUserById,
   updateUserById,
